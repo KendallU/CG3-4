19acc57302e1aae1fb950a1fc712fd279d8f35f2
KendallU <kendallunion@gmail.com>
2018-07-29 16:13:24 -0500

    Add project files.

---
+++ Calculator2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Calculator2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kendall's Calculator!");
+            /* variables
+             
+            int firstNumber;
+            int secondNumber;
+            int answer;
+            string operation;
+            */
+
+            /*gather user information
+             */
+
+            Console.WriteLine("What is your first number?");
+            Console.Write("Type number here:");
+            string firstNumber;
+            firstNumber = Console.ReadLine();
+
+            Console.WriteLine("What is your second number?");
+            Console.Write("Type number here:");
+            string secondNumber;
+            secondNumber = Console.ReadLine();
+
+            int firstNumb = int.Parse(firstNumber);
+            int secondNumb = int.Parse(secondNumber);
+            int answer;
+
+            /*user operations
+             */
+
+            Console.WriteLine("What do you want to do? Add, Subtract, Multipy, or Divide?");
+            string operation;
+            operation = Console.ReadLine();
+
+            if (operation == "Add")
+            {
+                answer = firstNumb + secondNumb;
+                Console.WriteLine(firstNumb + "+" + secondNumb + "=" + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "Subtract")
+            {
+                answer = firstNumb - secondNumb;
+                Console.WriteLine(firstNumb + "-" + secondNumb + "=" + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "Multiply")
+            {
+                answer = firstNumb * secondNumb;
+                Console.WriteLine(firstNumb + "*" + secondNumb + "=" + answer);
+                Console.ReadLine();
+            }
+            else if (operation == "Divide")
+            {
+                answer = firstNumb / secondNumb;
+                Console.WriteLine(firstNumb + "/" + secondNumb + "=" + answer);
+                Console.ReadLine();
+            }
+            else
+                    {
+                Console.Write("Sorry I don't understand!");
+                Console.ReadLine();
+            }
+            }
+        }
+    }
